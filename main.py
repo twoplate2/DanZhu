87a67b1302c1c606046bc36f821ac195381df531
unknown <liangpan@cn.net.ntes>
2026-07-28 17:23:27 +0800

    Align: info row left-padding 6dp, 珠子 left-align, matches RTP/bet labels

--- main.py
+++ main.py
@@ -2200,8 +2200,9 @@ class RootWidget(BoxLayout):
         self.add_widget(self.game_area)
         # ---- 信息区 ----
         # 信息行: 珠子 + 每次投x珠,累计x投x中(x%)
-        info = BoxLayout(size_hint_y=None, height=dp(H_INFO))
-        info.add_widget(self._mk_label("珠子：", "15sp", COL_TEXT, "right", True,
+        info = BoxLayout(size_hint_y=None, height=dp(H_INFO),
+                         padding=[dp(6), 0])
+        info.add_widget(self._mk_label("珠子：", "15sp", COL_TEXT, "left", True,
                                        size_hint_x=0.13))
         self.balance_lbl = self._mk_label(str(self.balance), "19sp", COL_BALL,
                                           "left", True, size_hint_x=0.17)
